3f5799f1073d76008a9feae9a6f5d603ee6b870a
GitHub Action <action@github.com>
2026-04-29 16:48:32 +0000

    Revert "fully revamped test suite"
    This reverts commit 3b59595d18bb161682c5a0a37a50bbaae549f9aa.

--- CommandStream.ts
+++ CommandStream.ts
@@ -68,13 +68,12 @@ export default class CommandStream {
   #inprogress?: Promise<Raw.Result>;
   #queue: Array<Raw.Chunk | undefined> = [];
   #notify?: () => void;
-
-  readonly factory: StreamFactory;
-  readonly dockerode: Dockerode;
+  #factory: StreamFactory;
+  #dockerode: Dockerode;
 
   constructor(dockerode: Dockerode, factory: StreamFactory) {
-    this.factory = factory;
-    this.dockerode = dockerode;
+    this.#factory = factory;
+    this.#dockerode = dockerode;
   }
 
   #open(): Promise<Raw.Result> {
@@ -83,7 +82,7 @@ export default class CommandStream {
 
   async #execute(): Promise<Raw.Result> {
     try {
-      const { stream, getExitCode, raw } = await this.factory();
+      const { stream, getExitCode, raw } = await this.#factory();
 
       const stdoutPass = new PassThrough();
       const stderrPass = new PassThrough();
@@ -100,7 +99,7 @@ export default class CommandStream {
       });
 
       if (raw) stream.pipe(stdoutPass);
-      else this.dockerode.modem.demuxStream(stream, stdoutPass, stderrPass);
+      else this.#dockerode.modem.demuxStream(stream, stdoutPass, stderrPass);
 
       await new Promise<void>((resolve, reject) => {
         stream.on("end", resolve);
